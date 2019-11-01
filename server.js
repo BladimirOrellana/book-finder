d60000afc1e19abae0a9c79be01d7c6d53cc454c
Bladimir Orellana <me@bladimirorellana.com>
2019-11-01 13:07:23 -0500

    kk

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require('path');
 
 const mongoose = require("mongoose");
 const routes = require("./routes");
@@ -11,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use('/static', express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
@@ -21,6 +20,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglis
 
 // Start the API server
 app.listen(PORT, function() {
-
+  
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
