afa83699a85ca4c054efd2b0e3480f84320444b6
Bladimir Orellana <me@bladimirorellana.com>
2019-11-01 11:55:49 -0500

    Updates

---
+++ precache-manifest.b12fc468ca73ca84611939a9a3d323da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "357bb069304e8084f3f70fa1288cb7b6",
+    "url": "/BladimirOrellana/book-finder/index.html"
+  },
+  {
+    "revision": "1fd271cf2527eab8b798",
+    "url": "/BladimirOrellana/book-finder/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fd1583239620c29e0816",
+    "url": "/BladimirOrellana/book-finder/static/js/2.34f9fee5.chunk.js"
+  },
+  {
+    "revision": "1fd271cf2527eab8b798",
+    "url": "/BladimirOrellana/book-finder/static/js/main.dc7854c6.chunk.js"
+  },
+  {
+    "revision": "79403dd3e128c5ed3b35",
+    "url": "/BladimirOrellana/book-finder/static/js/runtime-main.c717ebad.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/BladimirOrellana/book-finder/static/media/logo.25bf045c.svg"
+  }
+]);
